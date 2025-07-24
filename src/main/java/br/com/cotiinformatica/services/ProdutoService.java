6e57f0b5c5879a56c3a4a0a43c042c27e98c5a75
alison <adevpr11@gmail.com>
2025-07-24 11:00:09 -0300

    feat(service): adiciona métodos para criar e listar produtos na interface ProdutoService

--- src/main/java/br/com/cotiinformatica/services/ProdutoService.java
+++ src/main/java/br/com/cotiinformatica/services/ProdutoService.java
@@ -1,7 +1,16 @@
 package br.com.cotiinformatica.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import br.com.cotiinformatica.dtos.ProdutoRequest;
+import br.com.cotiinformatica.dtos.ProdutoResponse;
 
 @Service
 public interface ProdutoService {
+
+	ProdutoResponse create(ProdutoRequest request);
+
+	List<ProdutoResponse> findAll();
 }
