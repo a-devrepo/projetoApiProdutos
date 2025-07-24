5e36b9377443713aa71af4acc409c31013cca1fb
alison <adevpr11@gmail.com>
2025-07-24 10:17:34 -0300

    feat(produto): adiciona classe Produto

---
+++ src/main/java/br/com/cotiinformatica/entities/Produto.java
@@ -0,0 +1,62 @@
+package br.com.cotiinformatica.entities;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+	private String nome;
+	private Integer quantidade;
+	private Double preco;
+	
+	public Produto() {
+	}
+
+	public Produto(String nome, Integer quantidade, Double preco) {
+		this.nome = nome;
+		this.quantidade = quantidade;
+		this.preco = preco;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+}
