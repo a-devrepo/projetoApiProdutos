decbfb60b48aa6110339ab9656da4a0411a73832
alison <adevpr11@gmail.com>
2025-07-24 10:47:03 -0300

    feat(produto): adiciona validação via Bean Validation no DTO

--- src/main/java/br/com/cotiinformatica/dtos/ProdutoRequest.java
+++ src/main/java/br/com/cotiinformatica/dtos/ProdutoRequest.java
@@ -1,11 +1,24 @@
 package br.com.cotiinformatica.dtos;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class ProdutoRequest {
 
-	private String nome;
-	private Integer quantidade;
-	private Double preco;
+	@Size(min = 3, max = 50, message = "O nome do produto deve ter entre 3 e 50 caracteres.")
+    @NotBlank(message = "O nome do produto é obrigatório.")
+    private String nome;
+
+    @NotNull(message = "A quantidade é obrigatória.")
+    @Min(value = 1, message = "A quantidade deve ser maior que zero.")
+    private Integer quantidade;
+
+    @NotNull(message = "O preço é obrigatório.")
+    @Positive(message = "O preço deve ser maior que zero.")
+    private Double preco;
 }
